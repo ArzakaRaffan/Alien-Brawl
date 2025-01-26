54f179ed4d4a36993c183ab73ef6d94caaf61152
viscasa <attaralwie@gmail.com>
2025-01-26 10:25:38 +0700

    Finish music Minigame2

--- Main/Cutscene/cutscene.gd
+++ Main/Cutscene/cutscene.gd
@@ -1,16 +1,17 @@
 extends Node2D
-@onready var alien_ijo: MC = $AlienIjo
-@onready var transition: ColorRect = $CanvasLayer/Transition
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var transition: ColorRect = $CanvasLayer/Transition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	alien_ijo.start()
+	pass # Replace with function body.
+
+func goto_lobby():
 	transition.show()
-	transition.get_child(0).play("fade_in")
+	transition.get_child(0).play("fade_out")
 	await transition.get_child(0).animation_finished
-
-
+	get_tree().change_scene_to_file("res://Main/Lobby/lobby.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
