2877bcf0ac62d6f65cf3ce1ac852ce959a820878
arzakaraffan <arzakaraffan@gmail.com>
2025-01-26 08:41:55 +0700

    Minigame 1 & 3

--- Main/Minigame_1/Scenes/parallax_background.gd
+++ Main/Minigame_1/Scenes/parallax_background.gd
@@ -1,4 +1,6 @@
 extends ParallaxBackground
 
 func _process(delta: float) -> void:
-	$ParallaxLayer.motion_offset.y += $ParallaxLayer.motion_scale.y * 100 * delta
+	$ParallaxLayer.motion_offset.y += $ParallaxLayer.motion_scale.y * 150 * delta
+	$planet.motion_offset.y += $planet.motion_scale.y * 200 * delta
+	$star.motion_offset.y += $star.motion_scale.y * 75 * delta
